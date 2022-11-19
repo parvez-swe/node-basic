cd4f323df30b2123631330c6546d190e6b5a4603
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-11-19 22:59:02 +0600

    Async Awiat concept

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-//***Blocking, synchronous way */
+const http = require("http");
+const url = require("url");
+
+///////////////////////////////////////
+///////////////File
+
+//*****************Blocking, synchronous way */
 //read and write file by the "fs";
 const fs = require("fs");
 // const textIN = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -7,7 +13,7 @@ const fs = require("fs");
 // fs.writeFileSync("./txt/output.txt", textOut);
 // console.log("File Written!");
 
-///****None-blocking, Asynchronous way
+///************************None-blocking, Asynchronous way
 //sychronous means one after one do the work. it's also called blocking;
 //Asynchronous is not blocking  */
 
@@ -16,18 +22,35 @@ const fs = require("fs");
 // });
 // console.log('will read file!!!');
 
-fs.readFile('./txt/start.txt','utf-8',(err,data)=>{
-    fs.readFile(`./txt/${data}.txt`,'utf-8',(err,data2)=>{
-        console.log(data2);
-        fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
-            console.log(data3);
-            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err=>{
-                console.log('Your file has been writen😎');
-            })
-        })
-    })
-})
-console.log('will read file!');
+// fs.readFile('./txt/start.txt','utf-8',(err,data)=>{
+//     fs.readFile(`./txt/${data}.txt`,'utf-8',(err,data2)=>{
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
+//             console.log(data3);
+//             fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err=>{
+//                 console.log('Your file has been writen😎');
+//             })
+//         })
+//     })
+// })
+// console.log('will read file!');
 
+////////////////////////////////////
+///////////////SERVER
 
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("Hello from overview");
+  } else if (pathName === "/product") {
+    res.end("This is product page");
+  } else {
+    res.writeHead(404);
+    res.end("page not found");
+  }
 
+  //   console.log(req.url);
+});
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
