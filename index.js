57ce48344b6e012b82cf9bb7f41f9603c535d854
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-11-19 19:41:50 +0600

    Async Awiat concept

--- index.js
+++ index.js
@@ -16,6 +16,18 @@ const fs = require("fs");
 // });
 // console.log('will read file!!!');
 
+fs.readFile('./txt/start.txt','utf-8',(err,data)=>{
+    fs.readFile(`./txt/${data}.txt`,'utf-8',(err,data2)=>{
+        console.log(data2);
+        fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
+            console.log(data3);
+            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err=>{
+                console.log('Your file has been writen😎');
+            })
+        })
+    })
+})
+console.log('will read file!');
 
 
 
