64da31e9f0492bbe8b51a2ae89b1bbd42f559885
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-11-20 19:31:15 +0600

    dynamic route page have added!

--- index.js
+++ index.js
@@ -36,7 +36,24 @@ const fs = require("fs");
 // console.log('will read file!');
 
 ////////////////////////////////////
-///////////////
+///////////////server
+
+
+const replaceTemplate = (temp,product)=>{
+  let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+  output = output.replace(/{%IMAGE%}/g,product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g,product.from);
+  output = output.replace(/{%NUTRIENTS%}/g,product.from);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
+
+
+  return output;
+}
+
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
@@ -61,7 +78,9 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
       "Content-type": "text/html",
     });
-    res.end(tempOverview);
+    const cardsHtml = dataObj.map(el=>replaceTemplate(tempCard,el));
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml);
+    res.end(output);
 
     //PRODUCT PAGE
   } else if (pathName === "/product") {
