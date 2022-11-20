ffce710c4191c9fc8c903d566772624e13026543
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-11-20 19:33:45 +0600

    data object have added

--- index.js
+++ index.js
@@ -38,21 +38,20 @@ const fs = require("fs");
 ////////////////////////////////////
 ///////////////server
 
-
-const replaceTemplate = (temp,product)=>{
-  let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
-  output = output.replace(/{%IMAGE%}/g,product.image);
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
   output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g,product.from);
-  output = output.replace(/{%NUTRIENTS%}/g,product.from);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.from);
   output = output.replace(/{%QUANTITY%}/g, product.quantity);
   output = output.replace(/{%DESCRIPTION%}/g, product.description);
   output = output.replace(/{%ID%}/g, product.id);
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
-
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
 
   return output;
-}
+};
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -69,6 +68,7 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const productData = JSON.parse(data);
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
@@ -78,8 +78,8 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
       "Content-type": "text/html",
     });
-    const cardsHtml = dataObj.map(el=>replaceTemplate(tempCard,el));
-    const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml);
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el));
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
     res.end(output);
 
     //PRODUCT PAGE
