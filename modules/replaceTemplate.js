f24e6f1c70ad0b22ade7d5cfa700c87325b79f6e
Mohammad Parvez Musharaf <88303363+parvez2m@users.noreply.github.com>
2023-01-12 20:38:11 +0600

    Update replaceTemplate.js

--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -1,4 +1,5 @@
 module.exports = (temp, product) => {
+  let test;
   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
   output = output.replace(/{%IMAGE%}/g, product.image);
   output = output.replace(/{%PRICE%}/g, product.price);
